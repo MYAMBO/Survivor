ab76b65e64dff72556d0bbca5e3a1d95b662f0ee
MartinB-E <martin.bonte@epitech.eu>
2025-09-02 11:11:52 +0200

    feat(front): add .js and .css for main front

--- front/src/App/App.js
+++ front/src/App/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import Header from '../Header/Header';
+import Login from '../Log/Login';
+import SignUp from '../Log/Signup';
 
 function Home() {
   return (<div>home</div>)
@@ -14,14 +16,24 @@ function Contact() {
   return (<div>contact</div>)
 }
 
+function Log() {
+  return (<Login/>)
+}
+
+function SignUpLog() {
+  return (<SignUp/>)
+}
+
 function App() {
   return (
     <BrowserRouter>
       <Header/>
       <Routes>
         <Route path='/' element={<Home/>}/>
-        <Route path='/project' element={<Project/>}/>
-        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/login' element={<Log/>}/>
+        <Route path='/signup' element={<SignUpLog/>}/>
+        <Route path='/projects' element={<Project/>}/>
+        <Route path='/contacts' element={<Contact/>}/>
       </Routes>
     </BrowserRouter>
   );
--- front/src/Header/Header.js
+++ front/src/Header/Header.js
@@ -4,8 +4,10 @@ function Header() {
   return (
     <nav>
       <Link to="/">Home</Link>
-      <Link to="/project">Projects</Link>
-      <Link to="/contact">Contact</Link>
+      <Link to="/projects">Projects</Link>
+      <Link to="/contacts">Contacts</Link>
+      <Link to="/login">Log In</Link>
+      <Link to="/signup">Sign Up</Link>
     </nav>
   );
 }
