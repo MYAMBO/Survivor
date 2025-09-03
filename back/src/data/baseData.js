f75a01c35e545127bcb02dbbceb74035103cab91
YanisMonte <yanis.monte@epitech.eu>
2025-09-03 13:06:48 +0200

    feat(data): add findUserDataByName function

--- back/src/data/baseData.js
+++ back/src/data/baseData.js
@@ -71,4 +71,25 @@ async function findUserData(tableName, id) {
     }
 }
 
-module.exports = {writeUserData, readUserData, findUserData};
+async function findUserDataByName(tableName, name) {
+    const snapshot = await db.ref(tableName).once('value');
+    if (snapshot.exists()) {
+        const obj = snapshot.val()
+        const users = Object.entries(obj).map(([id, data]) => ({
+            id,
+            ...data
+        }));
+        const myStr = JSON.stringify(users, null, 0)
+        const myObj = JSON.parse(myStr)
+        for (const user of myObj){
+            if (user.username === name){
+                console.log(user.email);
+                return user;
+            }
+        }
+    } else {
+        console.log("Not found")
+    }
+}
+
+module.exports = {writeUserData, readUserData, findUserData, findUserDataByName};
