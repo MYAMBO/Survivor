d94e3231cac38dbeac2eb5bc8905a139bfca9866
MartinB-E <martin.bonte@epitech.eu>
2025-09-03 12:43:03 +0200

    feat(Signup): change signup for investor and funder

--- front/src/Log/Signup.js
+++ front/src/Log/Signup.js
@@ -4,32 +4,133 @@ import "./Login.css";
 import bcrypt from 'bcryptjs';
 
 function SignUp({ onSignUp }) {
+    const [isFunder, setIsFunder] = useState(true);
+
     const [formData, setFormData] = useState({
-        name: "",
-        legal_status: "",
-        address: "",
+        name : "",
         email: "",
-        phone: "",
-        sector: "",
-        maturity: "",
+        role : "",
         password: "",
         confirmPassword: ""
     });
 
-    const handleChange = (e) => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (formData.password !== formData.confirmPassword) {
+            alert("Passwords do not match");
+            return;
+        }
+
+        const hashedPassword = bcrypt.hashSync(formData.password, 10);  
+        fetch("http://10.17.72.24:3000/register", {
+            method: "POST",
+            headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                ...formData,
+                password: hashedPassword
+            }),
+        })
+        .then(res => res.json())
+        .then(data => {console.log("Inscription réussie :", data);})
+        .catch(err => {console.error("Erreur :", err);});
+    }
+
+    const handleChangeUser = (e) => {
         const { name, value } = e.target;
         setFormData((prev) => ({ ...prev, [name]: value }));
     };
 
-    const handleSubmit = (e) => {
+    const user_fields = [
+        { name: "name", type: "text", placeholder: "Name" },
+        { name: "email", type: "email", placeholder: "Email Address" },
+        { name: "password", type: "password", placeholder: "Password" },
+        { name: "confirmPassword", type: "password", placeholder: "Confirm Password" },
+    ];
+
+    return (
+        <div className="login-container">
+            <div className="back-home">
+                <Link to="/" className="home-button">Back to Home</Link>
+            </div>
+            <div className="website-logo">
+                <img src="logo.png" alt="Website Logo" className="logo-image"/>
+            </div>
+        <h2>Sign Up</h2>
+        <div className="toggle-container">
+        <div className={`slider ${isFunder ? "right" : "left"}`}></div>
+            <button
+                type="button"
+                className={!isFunder ? "active" : ""}
+                onClick={() => setIsFunder(false)}
+            >
+                Investor
+            </button>
+            <button
+                type="button"
+                className={isFunder ? "active" : ""}
+                onClick={() => setIsFunder(true)}
+            >
+                Funder
+            </button>
+        </div>
+
+        {isFunder ? (
+        formData.role = "Funder",
+        <form onSubmit={handleSubmit} className="signup-form">
+            {user_fields.map(({ name, type, placeholder }) => (
+            <input
+                key={name}
+                name={name}
+                type={type}
+                placeholder={placeholder}
+                value={formData[name]}
+                onChange={handleChangeUser}
+                required
+            />
+            ))}
+            <button type="submit">Create User Account</button>
+        </form>
+        ) : (
+        formData.role = "Investor",
+        <form onSubmit={handleSubmit} className="signup-form">
+            {user_fields.map(({ name, type, placeholder }) => (
+            <input
+                key={name}
+                name={name}
+                type={type}
+                placeholder={placeholder}
+                value={formData[name]}
+                onChange={handleChangeUser}
+                required
+            />
+            ))}
+            <button type="submit">Create User Account</button>
+        </form>
+        )}
+        <p className="signup-link">
+            Already have an account? <Link to="/login">Log in</Link>
+        </p>
+    </div>
+  );
+}
+
+export default SignUp;
+
+
+/*
+    const handleSubmitStartup = (e) => {
         e.preventDefault();
       
-        if (formData.password !== formData.confirmPassword) {
+        if (formDataStartup.password !== formDataStartup.confirmPassword) {
           alert("Passwords do not match");
           return;
         }
     
-        const hashedPassword = bcrypt.hashSync(formData.password, 10);  
+        const hashedPassword = bcrypt.hashSync(formDataStartup.password, 10);  
         fetch("http://10.17.71.123:3000/createStartup", {
           method: "POST",
           headers: {
@@ -42,16 +143,25 @@ function SignUp({ onSignUp }) {
           }),
         })
         .then(res => res.json())
-        .then(data => {
-          console.log("Inscription réussie :", data);
-        })
-        .catch(err => {
-          console.error("Erreur :", err);
-        });
+        .then(data => {console.log("Inscription réussie :", data);})
+        .catch(err => {console.error("Erreur :", err);});
     };
-      
 
-    const fields = [
+        const [formDataStartup, setFormDataStartup] = useState({
+        name: "",
+        legal_status: "",
+        address: "",
+        email: "",
+        phone: "",
+        sector: "",
+        maturity: "",
+        password: "",
+        confirmPassword: ""
+    });
+    const handleChangeStartup = (e) => {
+        const { name, value } = e.target;
+        setFormDataStartup((prev) => ({ ...prev, [name]: value }));
+    };    const startup_fields = [
         { name: "name", type: "text", placeholder: "Name" },
         { name: "legal_status", type: "text", placeholder: "Legal Status" },
         { name: "address", type: "text", placeholder: "Address" },
@@ -63,34 +173,4 @@ function SignUp({ onSignUp }) {
         { name: "confirmPassword", type: "password", placeholder: "Confirm Password" },
     ];
 
-    return (
-        <div className="login-container">
-        <div className="back-home">
-            <Link to="/" className="home-button">Back to Home</Link>
-        </div>
-        <div className="website-logo">
-            <img src="logo.png" alt="Website Logo" className="logo-image"/>
-        </div>
-        <h2>Sign Up</h2>
-        <form onSubmit={handleSubmit} className="login-form">
-            {fields.map(({ name, type, placeholder }) => (
-            <input
-                key={name}
-                name={name}
-                type={type}
-                placeholder={placeholder}
-                value={formData[name]}
-                onChange={handleChange}
-                required
-            />
-            ))}
-            <button type="submit">Create Account</button>
-        </form>
-        <p className="signup-link">
-            Already have an account? <Link to="/login">Log in</Link>
-        </p>
-        </div>
-  );
-}
-
-export default SignUp;
+    */
