aa4f0dff1d82e8bb8e8ba1b6438754e7df1dd471
YanisMonte <yanis.monte@epitech.eu>
2025-09-03 17:59:52 +0200

    feat(index): create documentation with swagger

--- back/index.js
+++ back/index.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 
 const express = require('express');
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 const app = express();
 const cookieParser = require('cookie-parser');
 const port = 3000
@@ -13,6 +15,27 @@ const loginRoutes = require('./src/routes/login');
 const logoutRoutes = require('./src/routes/logout');
 const startupsRoutes = require('./src/routes/startups');
 const userRoutes = require('./src/routes/users');
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "JEB API Documentation",
+      version: "1.0.0",
+      description: "swagger-jsdoc auto-generate documentation",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+      },
+    ],
+  },
+  apis: ["./src/routes/*.js"],
+};
+
+const swaggerSpec = swaggerJsdoc(options);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use(cors());
 app.use(cookieParser());
@@ -44,4 +67,6 @@ schedule.scheduleJob('0 */10 * * * *', async () => {
 });
 
 console.log(`App starting on port ${port}`);
+console.log(`Swagger Documentation: http://localhost:${port}/api-docs`);
+
 app.listen(port);
