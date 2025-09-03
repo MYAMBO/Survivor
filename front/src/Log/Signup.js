d445bd93f1121c5a5e32e11c954833bd0f4eb09d
ktimote <timote.koenig@epitech.eu>
2025-09-03 15:08:33 +0200

    fix: merge confict

--- front/src/Log/Signup.js
+++ front/src/Log/Signup.js
@@ -57,91 +57,63 @@ function SignUp({ onSignUp }) {
                 <Link to="/" className="home-button-text">Back to Home</Link>
                 <Link to="/" className="home-button-img"> <img src="./home.png" alt="Home Button"/> </Link>
             </div>
-                <img src="logo.png" alt="Website Logo" className="logo-image"/>
+            <img src="logo.png" alt="Website Logo" className="logo-image"/>
             <h2>Sign Up</h2>
-            <div className="login-form-section">
-                <form onSubmit={handleSubmit} className="login-form sign-up-form">
-                    {fields.map(({ name, type, placeholder }) => (
+            <div className="toggle-container">
+                <div className={`slider ${isFunder ? "right" : "left"}`}>
+                </div>
+                <button
+                    type="button"
+                    className={!isFunder ? "active" : ""}
+                    onClick={() => setIsFunder(false)}
+                >
+                    Investor
+                </button>
+                <button
+                    type="button"
+                    className={isFunder ? "active" : ""}
+                    onClick={() => setIsFunder(true)}
+                >
+                    Founder
+                </button>
+            </div>
+            {isFunder ? (formData.role = "founder",
+                <form onSubmit={handleSubmit} className="signup-form">
+                    {user_fields.map(({ name, type, placeholder }) => (
                         <input
                             key={name}
                             name={name}
                             type={type}
                             placeholder={placeholder}
                             value={formData[name]}
-                            onChange={handleChange}
+                            onChange={handleChangeUser}
                             required
                         />
                     ))}
+                    <button type="submit">Create User Account</button>
                 </form>
-                <button className="login-form-button">Create Account</button>
-            </div>
+            ) : (
+                formData.role = "investor",
+                <form onSubmit={handleSubmit} className="signup-form">
+                    {user_fields.map(({ name, type, placeholder }) => (
+                        <input
+                            key={name}
+                            name={name}
+                            type={type}
+                            placeholder={placeholder}
+                            value={formData[name]}
+                            onChange={handleChangeUser}
+                            required
+                        />
+                    ))}
+                    <button type="submit">Create User Account</button>
+                </form>
+            )}
             <p className="signup-link">
                 Already have an account? <Link to="/login">Log in</Link>
             </p>
         </div>
-
-                <Link to="/" className="home-button">Back to Home</Link>
-            </div>
-            <div className="website-logo">
-                <img src="logo.png" alt="Website Logo" className="logo-image"/>
-            </div>
-        <h2>Sign Up</h2>
-        <div className="toggle-container">
-        <div className={`slider ${isFunder ? "right" : "left"}`}></div>
-            <button
-                type="button"
-                className={!isFunder ? "active" : ""}
-                onClick={() => setIsFunder(false)}
-            >
-                Investor
-            </button>
-            <button
-                type="button"
-                className={isFunder ? "active" : ""}
-                onClick={() => setIsFunder(true)}
-            >
-                Founder
-            </button>
-        </div>
-
-        {isFunder ? (
-        formData.role = "founder",
-        <form onSubmit={handleSubmit} className="signup-form">
-            {user_fields.map(({ name, type, placeholder }) => (
-            <input
-                key={name}
-                name={name}
-                type={type}
-                placeholder={placeholder}
-                value={formData[name]}
-                onChange={handleChangeUser}
-                required
-            />
-            ))}
-            <button type="submit">Create User Account</button>
-        </form>
-        ) : (
-        formData.role = "investor",
-        <form onSubmit={handleSubmit} className="signup-form">
-            {user_fields.map(({ name, type, placeholder }) => (
-            <input
-                key={name}
-                name={name}
-                type={type}
-                placeholder={placeholder}
-                value={formData[name]}
-                onChange={handleChangeUser}
-                required
-            />
-            ))}
-            <button type="submit">Create User Account</button>
-        </form>
-        )}
-        <p className="signup-link">
-            Already have an account? <Link to="/login">Log in</Link>
-        </p>
-    </div>
-  );
+    );
 }
 
 export default SignUp;
