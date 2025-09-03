814f4d93dc59f4602f816cc9757a218c5658d6b0
YanisMonte <yanis.monte@epitech.eu>
2025-09-03 16:41:49 +0200

    refractor(routes): set routes in differents files

--- back/src/routes/startups.js
+++ back/src/routes/startups.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const authenticateToken = require('../middleware/authMiddleware');
 const router = express.Router();
 const createStartup = require("../data/startupsManagement");
 
