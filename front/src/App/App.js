ef063424e2fe4eddcd583ceddf1a401549dddf8f
MartinB-E <martin.bonte@epitech.eu>
2025-09-10 12:30:14 +0200

    feat(dashboard): stats on dashboar

--- front/src/App/App.js
+++ front/src/App/App.js
@@ -54,6 +54,7 @@ function App() {
             <>
               <Route path='/login' element={<Login/>}/>
               <Route path='/signup' element={<SignUp/>}/>
+              <Route path='/dashboard' element={<Dashboard/>}/>
             </>
           )}
           {(role === 'investor' || role === 'admin' || role === 'founder') && (
@@ -65,9 +66,11 @@ function App() {
               <Route path='/opportunities' element={<Opportunities/>}/>
             </>
           )}
+          {/**
+           * 
           {role === 'admin' && (
-            <Route path='/dashboard' element={<Dashboard/>}/>
           )}
+          */}
         </Routes>
       </BrowserRouter>
     );
