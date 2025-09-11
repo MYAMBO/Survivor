5bc50e4f00abbf4f0f16921a214c4b195fd41094
MartinB-E <martin.bonte@epitech.eu>
2025-09-11 09:28:17 +0200

    fix(App): fix root when log in

---
+++ front/src/Messaging/Messaging.js
@@ -0,0 +1,13 @@
+import './Messaging.css'
+
+function Messaging() {
+  return (
+    <div className="messaging">
+      <h2>Messaging Feature Coming Soon!</h2>
+      <p>We're working hard to bring you this feature. Stay tuned!</p>
+    </div>
+  )
+}
+
+export default Messaging;
+
